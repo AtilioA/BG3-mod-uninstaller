7b70ce55fb96723cf0b54fa5568e087a016d6c1e
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-25 16:55:36 -0300

    refactor: print item owner if it exists, add timer for container deletion just in case

--- Mod Uninstaller/Mods/ModUninstaller/ScriptExtender/Lua/Server/DeleteTemplates.lua
+++ Mod Uninstaller/Mods/ModUninstaller/ScriptExtender/Lua/Server/DeleteTemplates.lua
@@ -6,15 +6,32 @@ function MoveAllItemsFromContainer(containerUUID)
         if item.Guid then
             local exact, total = Osi.GetStackAmount(item.Guid)
             Osi.ToInventory(item.Guid, campChest, total)
+            MUPrint(0, "Moved item: " .. item.Guid .. " (" .. item.Name .. ") to camp chest.")
         end
     end
+
+    --- Delete the container after 1 second, just to be safe (in case the items are not moved in time)
+    VCHelpers.Timer:OnTime(1000, function()
+        Osi.RequestDelete(containerUUID)
+    end)
 end
 
 function DeleteAllMatchingTemplates(entities, templateID)
     for _, entity in pairs(entities) do
         if entity and entity.ServerItem and entity.ServerItem.Template and entity.ServerItem.Template.Id == templateID then
             if Osi.IsContainer(entity.Uuid.EntityUuid) == 1 then
+                MUWarn(0, "Container found with UUID: " .. entity.Uuid.EntityUuid .. ". Moving its items to camp chest.")
                 MoveAllItemsFromContainer(entity.Uuid.EntityUuid)
+                return
+            end
+            local itemOwner = VCHelpers.Inventory:GetHolder(entity.Uuid.EntityUuid)
+            if itemOwner then
+                MUWarn(0,
+                    "Item found in inventory of: " ..
+                    VCHelpers.Loca:GetDisplayName(itemOwner.Uuid.EntityUuid) ..
+                    " (" ..
+                    itemOwner.Uuid.EntityUuid ..
+                    ").")
             end
             MUWarn(0,
                 "Deleting entity: " .. entity.ServerItem.Template.Name .. " with UUID: " .. entity.Uuid.EntityUuid)
@@ -28,6 +45,7 @@ end
 function DeleteTemplatesForMod(modsTemplatesData)
     local entities = Ext.Entity.GetAllEntitiesWithComponent("ServerItem")
     for _, templateData in pairs(modsTemplatesData) do
+        MUWarn(2, "Processing template ID: " .. templateData.Id)
         DeleteAllMatchingTemplates(entities, templateData.Id)
     end
 end
